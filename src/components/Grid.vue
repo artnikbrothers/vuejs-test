7b72853ecb8052a7617e76efe667913822da69bd
Arthur Gavrilov <artnikbrothers@gmail.com>
2017-06-26 14:33:30 +0300

    Improve css grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="grid">
+  <div class="grid" :class="`grid-${columns}`">
     <slot></slot>
-    <div v-for="placeholder in placeholders" :class="`placeholders placeholder-${placeholder}`"></div>
+    <div v-for="column in columns" :class="`placeholders placeholder-${columns}`"></div>
   </div>
 </template>
 
@@ -9,10 +9,64 @@
 export default {
   name: 'Grid',
   props: {
-    placeholders: {
+    columns: {
       type: Number,
       default: 3
     }
   }
 }
 </script>
+
+<style lang="sass" scoped>
+@import '../assets/styles/variables'
+@import '../assets/styles/mixins'
+
+.grid
+  +center
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  flex-wrap: wrap
+  width: 100%
+
+  .column
+    margin: 10px 2px 15px
+
+  .column,
+  .placeholders
+    width: 45.1%
+    padding: 0
+    flex-basis: auto
+    flex-grow: 0
+
+    @media (min-width: $break-2)
+      margin-left: 3px
+      margin-right: 3px
+      max-width: none
+      width: 46.38%
+
+
+.grid-5
+  .column,
+  .placeholders
+    @media (min-width: $break-3)
+      width: 30.7%
+
+    @media (min-width: $break-4)
+      width: 22.4%
+
+    @media (min-width: $break-5)
+      width: 17.7%
+
+.grid-4
+  .column,
+  .placeholders
+    @media (min-width: $break-3)
+      width: 22.4%
+
+.grid-3
+  .column,
+  .placeholders
+    @media (min-width: $break-3)
+      width: 30.7%
+</style>
